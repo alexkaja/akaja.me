4a2361694dc8ed27daf629e7609f6443b6f7459b
alexkaja <akaja90@gmail.com>
2015-02-06 01:27:25 -0500

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
     resources :posts do
       resources :comments
     end
-    
+
     root 'posts#index'
+
+    get '/about', to: 'pages#about'
+    get '/resume', to: 'pages#resume'
 
 end
